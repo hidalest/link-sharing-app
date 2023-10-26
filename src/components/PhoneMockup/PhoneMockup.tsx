351b38e07697c280678f6b586598cd5a1b41b5b3
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2023-10-25 20:04:10 -0400

    fix: minor css adjustments

--- src/components/PhoneMockup/PhoneMockup.tsx
+++ src/components/PhoneMockup/PhoneMockup.tsx
@@ -43,39 +43,44 @@ export const PhoneMockup = ({
           </article>
         </section>
       )}
-      <section className={styles.linkContainer}>
-        {userLinks
-          .filter((link) => link.isValid)
-          .map(
-            ({ name, userLink, icon, backgroundColor, fontColor }, index) => {
-              return (
-                <a
-                  className={styles.link}
-                  href={userLink}
-                  target='_blank'
-                  key={index}
-                  style={{ backgroundColor }}
-                >
-                  <div className={styles.linkInfoContainer}>
-                    <SVGWrapper markup={icon} color={fontColor || '#ffffff'} />
-                    <span
-                      style={{ color: fontColor }}
-                      className={styles.platformName}
-                    >
-                      {name}
-                    </span>
-                  </div>
-                  <div>
-                    <SVGWrapper
-                      markup={arrowIconLinks}
-                      color={fontColor || '#ffffff'}
-                    />
-                  </div>
-                </a>
-              );
-            }
-          )}
-      </section>
+      {userLinks.length !== 0 && (
+        <section className={styles.linkContainer}>
+          {userLinks
+            .filter((link) => link.isValid)
+            .map(
+              ({ name, userLink, icon, backgroundColor, fontColor }, index) => {
+                return (
+                  <a
+                    className={styles.link}
+                    href={userLink}
+                    target='_blank'
+                    key={index}
+                    style={{ backgroundColor }}
+                  >
+                    <div className={styles.linkInfoContainer}>
+                      <SVGWrapper
+                        markup={icon}
+                        color={fontColor || '#ffffff'}
+                      />
+                      <span
+                        style={{ color: fontColor }}
+                        className={styles.platformName}
+                      >
+                        {name}
+                      </span>
+                    </div>
+                    <div>
+                      <SVGWrapper
+                        markup={arrowIconLinks}
+                        color={fontColor || '#ffffff'}
+                      />
+                    </div>
+                  </a>
+                );
+              }
+            )}
+        </section>
+      )}
     </Card>
   );
 };
